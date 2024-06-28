91412a4914a677ef9a42d77ff849b6797bea69c5
Garbi93 <iseung2093@gmail.com>
2024-06-28 16:10:37 +0900

    06/28 메인페이지 로그인, 비로그인 보여줄 틀 구성

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,32 @@
 import React from "react";
 import BasicLayout from "../layouts/BasicLayout";
-import { Card, Flex } from "@chakra-ui/react";
+import { Card } from "@chakra-ui/react";
 
 function MainPage() {
+  // TODO: 비 로그인시 보여줄 페이지 (로그인 기능 추가 후 권한 분기 시켜줄때 활성화)
+  // return (
+  //   <BasicLayout>
+  //     <Flex justifyContent="center" mt="90px">
+  //       <Card
+  //         w="370px"
+  //         h="300px"
+  //         bg="gray.300"
+  //         fontSize="4xl"
+  //         fontWeight="bold"
+  //         textAlign="center"
+  //         lineHeight="300px"
+  //       >
+  //         메인 페이지 입니다.
+  //       </Card>
+  //     </Flex>
+  //   </BasicLayout>
+  // );
+
+  // 로그인 유저 메인 페이지 -------------------------------
   return (
     <BasicLayout>
-      <Flex justifyContent="center" mt="90px">
+      <Card justifyContent="center" alignItems="center" mt="90px">
+        로그인 유저 메인페이지 영역
         <Card
           w="370px"
           h="300px"
@@ -15,11 +36,24 @@ function MainPage() {
           textAlign="center"
           lineHeight="300px"
         >
-          메인 페이지 입니다.
+          지출비율 영역
         </Card>
-      </Flex>
+        <Card
+          w="370px"
+          h="90px"
+          mt="20px"
+          bg="gray.300"
+          fontSize="4xl"
+          fontWeight="bold"
+          textAlign="center"
+          lineHeight="90px"
+        >
+          수입 비율 영역
+        </Card>
+      </Card>
     </BasicLayout>
   );
+  // 로그인 유저 메인 페이지 -------------------------------
 }
 
 export default MainPage;
