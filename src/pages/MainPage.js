76922c29a5f12a5a1dcf1456aa58c7548ec425a8
Garbi93 <iseung2093@gmail.com>
2024-06-28 16:02:35 +0900

    06/28 메인페이지 ui 틀 잡기

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,23 @@
 import React from "react";
 import BasicLayout from "../layouts/BasicLayout";
-import { Box } from "@chakra-ui/react";
+import { Card, Flex } from "@chakra-ui/react";
 
 function MainPage() {
   return (
     <BasicLayout>
-      <Box style={{ fontSize: "xx-large" }}>main page</Box>
+      <Flex justifyContent="center" mt="90px">
+        <Card
+          w="370px"
+          h="300px"
+          bg="gray.300"
+          fontSize="4xl"
+          fontWeight="bold"
+          textAlign="center"
+          lineHeight="300px"
+        >
+          메인 페이지 입니다.
+        </Card>
+      </Flex>
     </BasicLayout>
   );
 }
